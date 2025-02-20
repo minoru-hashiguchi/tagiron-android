cc67a8951b63c19bb4703923f8751268686fe0dd
MINORUHASHI576F\minoruhashiguchi <minoru.hashiguchi@icloud.com>
2025-02-20 19:55:12 +0900

    v2更新

--- tagiron-mobile/app/build.gradle.kts
+++ tagiron-mobile/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.icloud.hashiguchi.tagironmobile"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.9.0"
+        versionCode = 2
+        versionName = "0.9.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

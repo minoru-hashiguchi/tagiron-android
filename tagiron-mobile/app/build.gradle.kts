22857194279cd1fcfca039211cd96b00b93229e5
MINORUHASHI576F\minoruhashiguchi <minoru.hashiguchi@icloud.com>
2025-02-25 14:54:57 +0900

    v9 (0.9.7) 2025/02/25 ・スピナーの選択機能が動作していなかった不具合を修正 ・コンピュータレベルの設定をゲーム画面に表示するように修正

--- tagiron-mobile/app/build.gradle.kts
+++ tagiron-mobile/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.icloud.hashiguchi.tagironmobile"
         minSdk = 28
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.9.6"
+        versionCode = 9
+        versionName = "0.9.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
